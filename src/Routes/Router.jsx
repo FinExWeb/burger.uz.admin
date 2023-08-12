e056eb72fe0d3e5beadb559708fef20b02dd3599
FinExWeb <akhmedovweb@gmail.com>
2023-08-12 16:06:05 +0500

    first commit

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from "react-router-dom";
+import Home from '../Components/Home'
+
+const Routers = () => {
+    return(
+        <Routes>
+            <Route path="/" element={<Home />}/>
+        </Routes>
+    )
+}
